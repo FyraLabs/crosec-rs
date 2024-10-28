10417b139af20bfa48700ff47e203692d73dff7c
Lleyton Gray <lleyton@fyralabs.com>
2024-10-27 19:10:44 -0700

    fix: use lossy conversion

--- ectool/src/charge_current_limit_subcommand.rs
+++ ectool/src/charge_current_limit_subcommand.rs
@@ -6,6 +6,6 @@ use uom::si::electric_current::{milliampere, ElectricCurrent};
 
 pub fn charge_current_limit_subcommand(limit: u32) -> Result<()> {
     let mut file = File::open(CROS_EC_PATH)?;
-    set_charge_current_limit(&mut file, ElectricCurrent::new::<milliampere>(limit.into()))?;
+    set_charge_current_limit(&mut file, ElectricCurrent::new::<milliampere>(limit as f32))?;
     Ok(())
 }
